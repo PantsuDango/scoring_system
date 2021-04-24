2d770823eaa420d8163d116ecebb4eb96798d74b
PantsuDango <394883561@qq.com>
2021-04-24 12:12:34 +0800

    改名

--- git.sh
+++ git.sh
@@ -9,7 +9,7 @@ if [ $num -eq 1 ]; then
     kill -9 $(ps -ef | grep "scoring" | grep -v "grep" | awk '{print $2}')
     sleep 3
 fi
-nohup go run app.go &
+nohup go run scoring.go &
 sleep 3
 
 num=$(netstat -nlpt | grep ":::3000" | grep -v "grep" | wc -l)
